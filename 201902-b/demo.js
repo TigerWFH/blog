3b50f18545a2083c5a70dc8f6612fb86fdcbfdbb
monkey <monkey>
2019-03-12 22:11:51 +0800

    初步实现插入算法

--- 201902-b/demo.js
+++ 201902-b/demo.js
@@ -1,4 +1,4 @@
-// const mockData = [1, 34, 56, 2, 45, 78, 32, 7, 10, 11];
+const mockData = [1, 34, 56, 2, 45, 78, 32, 7, 10, 11];
 /*
 完全树结构：
 1
@@ -7,7 +7,7 @@
 7   10  11
  */
 // const mockData = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
-const mockData = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ];
+// const mockData = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ];
 
 function swap(a, left, right) {
     let tmp = a[left];
@@ -15,35 +15,18 @@ function swap(a, left, right) {
     a[right] = tmp;
 }
 
-function adjustHeap(a, end/*待调整元素索引 */) {
-    let middle = Math.floor(end / 2);
-    while(middle >= 0) {
-        if (middle * 2 + 1 <= end) {
-            if (a[middle * 2 + 1] > a[middle]) {
-                swap(a, middle * 2 + 1, middle);
-            }
-            if (a[middle * 2] > a[middle]) {
-                swap(a, middle * 2, middle);
-            }
-        }
-        else if (middle * 2 <= end) {
-            if (a[middle * 2] > a[middle]) {
-                swap(a, middle * 2, middle);
-            }
-        }
 
-        middle--;
-    }
-}
-
-function heapSort(a) {
+function insertSort(a) {
     if (!Array.isArray(a)) {
         return;
     }
-    for (let i = a.length - 1; i >= 0; i--) {
-        adjustHeap(a, i );
-        swap(a, 0, i);
+    for (let i = 1; i < a.length; i++) {
+        for (let j = i; j > 0; j--) {
+            if (a[j] < a[j - 1] ) {
+                swap(a, j - 1, j);
+            }
+        }
     }
 }
-heapSort(mockData);
-console.log("arr===heapSort===>", mockData);
+insertSort(mockData);
+console.log("arr===insertSort===>", mockData);
