d364dd6607521f8c2c6cfb4b53df12b625dd989e
monkey <monkey>
2019-03-20 22:39:18 +0800

    希尔排序，要注意希尔排序增量的选取

--- 201902-b/demo.js
+++ 201902-b/demo.js
@@ -1,4 +1,4 @@
-const mockData = [1, 34, 56, 2, 45, 78, 32, 7, 10, 11];
+// const mockData = [1, 34, 56, 2, 45, 78, 32, 7, 10, 11];
 /*
 完全树结构：
 1
@@ -7,7 +7,7 @@ const mockData = [1, 34, 56, 2, 45, 78, 32, 7, 10, 11];
 7   10  11
  */
 // const mockData = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
-// const mockData = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ];
+const mockData = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ];
 
 function swap(a, left, right) {
     let tmp = a[left];
@@ -15,37 +15,22 @@ function swap(a, left, right) {
     a[right] = tmp;
 }
 
-function insertSort(a) {
-    if (!Array.isArray(a)) {
-        return ;
-    }
-
-    for (let i = 1; i < a.length; i++) {
-        let value = a[i];
-        let index = i;
-        if (value < a[i - 1]) {
-            for (let j = i; j >= 0; j--) {
-                if (value < a[j]) {
-                    swap(a, j, index);
-                    index--;
-                }
-                console.log("a===>", a);
+function shellSort(a) {
+    let increment = a.length;
+    do {
+        increment = Math.floor(increment / 3) + 1;
+        for (let i = increment; i < a.length; i += increment) {
+            let tmp = a[i];
+            let position = i;
+            if (a[i] < a[i - increment]) {
+                do {
+                    swap(a, position, position - increment);
+                    position -= increment;
+                } while (position -increment >= 0 && tmp < a[position - increment]);
+                a[position] = tmp;
             }
         }
-        a[index] = value;
-    }
+    } while (increment > 1)
 }
-// function insertSort(a) {
-//     if (!Array.isArray(a)) {
-//         return;
-//     }
-//     for (let i = 1; i < a.length; i++) {
-//         for (let j = i; j > 0; j--) {
-//             if (a[j] < a[j - 1] ) {
-//                 swap(a, j - 1, j);
-//             }
-//         }
-//     }
-// }
-insertSort(mockData);
+shellSort(mockData);
 console.log("arr===insertSort===>", mockData);
